f93886df419c5eb5edf06cc0820aa2b5faf51e7c
Ruh <ruh@tut.by>
2026-09-25 14:00:28 +0300

    init: add main layout, header, sidebar and common css

---
+++ src/ServicePortal.UI/Models/AppShellState.cs
@@ -0,0 +1,26 @@
+﻿namespace ServicePortal.UI.Models
+{
+	public class AppShellState
+	{
+		/// <summary>
+		/// Application name.
+		/// </summary>
+		public string ApplicationName { get; set; } = string.Empty;
+
+		/// <summary>
+		/// Application logo.
+		/// </summary>
+		public string? ApplicatinLogo { get; set; }
+
+		/// <summary>
+		/// Services.
+		/// </summary>
+		public List<NavigationItem> NavigationItems { get; set; } = [];
+
+		/// <summary>
+		/// Active application.
+		/// </summary>
+		public string? ActiveItem { get; set; }
+
+	}
+}
---
+++ src/ServicePortal.UI/Models/NavigationItem.cs
@@ -0,0 +1,25 @@
+﻿namespace ServicePortal.UI.Models
+{
+	public class NavigationItem
+	{
+		/// <summary>
+		/// Service Id.
+		/// </summary>
+		int Id { get; set; }
+
+		/// <summary>
+		/// Service name.
+		/// </summary>
+		public string? Name { get; set; }
+
+		/// <summary>
+		/// Service url.
+		/// </summary>
+		public string? Url { get; set; }
+
+		/// <summary>
+		/// Service ico.
+		/// </summary>
+		public string? Icon { get; set; }
+	}
+}
